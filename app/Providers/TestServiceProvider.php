a3c7d35859950f170836cf2ff5c1bb4e1982b762
Zhiqiang Niu <nzqpeace@gmail.com>
2015-07-14 17:24:16 +0800

    init project

---
+++ app/Providers/TestServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace smarthome\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class TestServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
